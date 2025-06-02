921b4988050c2e56ce9ba0f4beca9c6e2f34a9cf
Prionta Ghosh <prionta.ghosh@sa.stud.vu.lt>
2025-06-02 23:11:41 +0300

    Send indexed word count from AgentA to Master

--- AgentA/Program.cs
+++ AgentA/Program.cs
@@ -1,11 +1,45 @@
 ﻿using System;
+using System.IO;
 using System.IO.Pipes;
 using System.Text;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Enter the folder path that contains .txt files:");
+        string? folderPath = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(folderPath))
+        {
+            Console.WriteLine("No folder path entered. Exiting.");
+            return;
+        }
+
+        string[] txtFiles = Directory.GetFiles(folderPath, "*.txt");
+
+        Dictionary<string, Dictionary<string, int>> fileWordCounts = new Dictionary<string, Dictionary<string, int>>();
+
+        foreach (string file in txtFiles)
+        {
+            string content = File.ReadAllText(file);
+            string[] words = content.Split(new char[] { ' ', '.', ',', '?', '!', ';', ':', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string cleanedWord = word.ToLower();
+                if (wordCount.ContainsKey(cleanedWord))
+                    wordCount[cleanedWord]++;
+                else
+                    wordCount[cleanedWord] = 1;
+            }
+
+            fileWordCounts[Path.GetFileName(file)] = wordCount;
+        }
+
         try
         {
             using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "agentApipe", PipeDirection.Out))
@@ -13,11 +47,18 @@ class Program
                 Console.WriteLine("Connecting to Master...");
                 pipeClient.Connect();
 
-                string testMessage = "Hello from AgentA!";
-                byte[] messageBytes = Encoding.UTF8.GetBytes(testMessage);
+                StringBuilder sb = new StringBuilder();
+                foreach (var fileEntry in fileWordCounts)
+                {
+                    foreach (var wordEntry in fileEntry.Value)
+                    {
+                        sb.AppendLine($"{fileEntry.Key}:{wordEntry.Key}:{wordEntry.Value}");
+                    }
+                }
 
+                byte[] messageBytes = Encoding.UTF8.GetBytes(sb.ToString());
                 pipeClient.Write(messageBytes, 0, messageBytes.Length);
-                Console.WriteLine("Message sent to Master.");
+                Console.WriteLine("Word count data sent to Master.");
             }
         }
         catch (Exception ex)
