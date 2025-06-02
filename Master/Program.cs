06929dd806b5064e6769166745129c4658460352
Prionta Ghosh <prionta.ghosh@sa.stud.vu.lt>
2025-06-02 22:31:19 +0300

    Receive message from AgentA via pipe in Master

--- Master/Program.cs
+++ Master/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello from Master");
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("agentApipe", PipeDirection.In))
+            {
+                Console.WriteLine("Waiting for AgentA to connect...");
+
+                pipeServer.WaitForConnection();
+                Console.WriteLine("AgentA connected.");
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = pipeServer.Read(buffer, 0, buffer.Length);
+
+                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received from AgentA: " + receivedMessage);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error in Master: " + ex.Message);
+        }
+    }
+}
