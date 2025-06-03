a80a91b9bf79f0078c751ce88b3ed6e115fe188c
Prionta Ghosh <prionta.ghosh@sa.stud.vu.lt>
2025-06-03 23:27:05 +0300

    Implement HandlePipe method for multithreaded pipe reading

--- Master/Program.cs
+++ Master/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO.Pipes;
 using System.Text;
+using System.Threading;
 
 class Program
 {
@@ -14,5 +15,28 @@ class Program
 
         agentAThread.Join();
         agentBThread.Join();
+    }
+
+    static void HandlePipe(string pipeName, string agentLabel)
+    {
+        try
+        {
+            using (NamedPipeServerStream pipeServer = new NamedPipeServerStream(pipeName, PipeDirection.In))
+            {
+                Console.WriteLine($"{agentLabel}: Waiting for connection on pipe '{pipeName}'...");
+                pipeServer.WaitForConnection();
+                Console.WriteLine($"{agentLabel}: Connected.");
+
+                byte[] buffer = new byte[4096];
+                int bytesRead = pipeServer.Read(buffer, 0, buffer.Length);
+                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine($"{agentLabel}: Received Data:\n{receivedMessage}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{agentLabel} Error: {ex.Message}");
+        }
     }
 }
