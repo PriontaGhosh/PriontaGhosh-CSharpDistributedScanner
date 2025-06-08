cba78672d8fb09c84494c0fadf647a8c172e727a
Prionta Ghosh <prionta.ghosh@sa.stud.vu.lt>
2025-06-08 08:59:49 +0300

    cleaned Main() and removed old logic to get ready for threading in AgentA

--- AgentA/Program.cs
+++ AgentA/Program.cs
@@ -21,50 +21,15 @@ static string folderPath = "";
         // run this agent on CPU core 0
         Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)(1 << 0);
         Console.WriteLine("Enter the folder path that contains .txt files:");
-        string? folderPath = Console.ReadLine();
+        folderPath = Console.ReadLine();  // ✅ now updates the global variable
 
         if (string.IsNullOrWhiteSpace(folderPath))
         {
             Console.WriteLine("No folder path entered. Exiting.");
             return;
         }
-        // split the file list between AgentA and AgentB so both don't scan same files
 
-        bool isAgentA = true; // manually define this for AgentA
-
-        string[] allFiles = Directory.GetFiles(folderPath, "*.txt");
-        int total = allFiles.Length;
-        int mid = total / 2;
-
-        string[] filesToProcess;
-
-        if (isAgentA)
-            filesToProcess = allFiles.Take(mid).ToArray(); // AgentA takes first half
-        else
-            filesToProcess = allFiles.Skip(mid).ToArray();
-
-
-        Dictionary<string, Dictionary<string, int>> fileWordCounts = new Dictionary<string, Dictionary<string, int>>();
-
-        foreach (string file in filesToProcess)
-        {
-            string content = File.ReadAllText(file);
-            string[] words = content.Split(new char[] { ' ', '.', ',', '?', '!', ';', ':', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-
-            Dictionary<string, int> wordCount = new Dictionary<string, int>();
-
-            foreach (string word in words)
-            {
-                string cleanedWord = word.ToLower();
-                if (wordCount.ContainsKey(cleanedWord))
-                    wordCount[cleanedWord]++;
-                else
-                    wordCount[cleanedWord] = 1;
-            }
-
-            fileWordCounts[Path.GetFileName(file)] = wordCount;
-        }
-
+       
         try
         {
             using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "agentApipe", PipeDirection.Out))
